23c030c5b3d2d05c550c567231f925b54c153734
GenKet <artemkot2281@gmail.com>
2023-04-05 16:42:25 +0300

    Delet recursion

--- general.py
+++ general.py
@@ -1,23 +1,23 @@
-# from main import dp
-# from main import bot
-# from main import BotDB
-# from aiogram.dispatcher.filters.state import State, StatesGroup
-# from aiogram.dispatcher.filters import Text
-# from aiogram.dispatcher import FSMContext
-# from aiogram import types
-# import requests
-#
-# @dp.message_handler(Text(equals="Перейти в меню"))
-# async def menu(message: types.Message):
-#     user_id = message.from_user.id
-#     if BotDB.executor_exists(user_id):
-#         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-#         item1 = types.KeyboardButton("Баланс", reply_markup=markup)
-#         item2 = types.KeyboardButton("Создать анкету", reply_markup=markup)
-#         item3 = types.KeyboardButton("Поиск заказов", reply_markup=markup)
-#         markup.add(item1, item2, item3)
-#         await  bot.send_message(message.chat.id,"Меню",reply_markup=markup)
-#
-#
-# 
-#
+from main import dp
+from main import bot
+from main import BotDB
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher import FSMContext
+from aiogram import types
+import requests
+
+@dp.message_handler(Text(equals="Перейти в меню"))
+async def menu(message: types.Message):
+    user_id = message.from_user.id
+    if BotDB.executor_exists(user_id):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton("Баланс")
+        item2 = types.KeyboardButton("Создать анкету")
+        item3 = types.KeyboardButton("Поиск заказов")
+        markup.add(item1, item2, item3)
+        await  bot.send_message(message.chat.id,"Меню",reply_markup=markup)
+
+
+
+
--- main.py
+++ main.py
@@ -36,19 +36,6 @@ async def help(message: types.Message):
     await message.answer(
         "Список доступных команд:")
 
-@dp.message_handler(Text(equals="Перейти в меню"))
-async def menu(message: types.Message):
-    user_id = message.from_user.id
-    if BotDB.executor_exists(user_id):
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        item1 = types.KeyboardButton("Баланс", reply_markup=markup)
-        item2 = types.KeyboardButton("Создать анкету", reply_markup=markup)
-        item3 = types.KeyboardButton("Поиск заказов", reply_markup=markup)
-        markup.add(item1, item2, item3)
-        await  bot.send_message(message.chat.id,"Меню",reply_markup=markup)
-
-
-
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
